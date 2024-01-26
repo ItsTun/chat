0ff581fd4a637ad2eee59132074f0d45f5908bdf
Tun Han <tunhanmra@rebbiz.com>
2024-01-26 18:03:33 +0630

    added handleSetUserName, handleSendMessage functionality for main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { TextField, Button, Container, Paper, AppBar, Toolbar, Typography } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { setUserName, sendMessage, loadMessageHistory } from './actions';
+
+const App = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector((state) => state.userName);
+  const messages = useSelector((state) => state.messages);
+
+  const [messageInput, setMessageInput] = useState('');
+
+  useEffect(() => {
+    dispatch(loadMessageHistory());
+  }, [dispatch]);
+
+  const handleSetUserName = () => {
+    const name = prompt('Enter your name:');
+    dispatch(setUserName(name));
+  };
+
+  const handleSendMessage = () => {
+    dispatch(sendMessage(messageInput));
+    setMessageInput('');
+  };
+
+};
 
 export default App;
